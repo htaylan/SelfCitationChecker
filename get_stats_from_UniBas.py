f6da5cfa525d86fe6d0bf845154f2c0c66dfa96c
htaylan <htaylanturan@gmail.com>
2023-03-15 12:56:16 +0100

    Update get_stats_from_UniBas.py

--- get_stats_from_UniBas.py
+++ get_stats_from_UniBas.py
@@ -1,44 +1,47 @@
-Can you make this code OOP based and more professional
-
 import re 
 import os 
 import requests
 from bs4 import BeautifulSoup
 
-def get_researcher_tables(url):
-    html = requests.get(url, headers=headers, proxies=proxies).text
-    soup = BeautifulSoup(html)
-    return soup
-  
-  def get_prof_names_from_uni(soup):
-    whole_table = str(soup.find('div', attrs={"id" : "collapse-1652"}))
-    return whole_table
- def get_postdoc_names_from_uni(soup):
-    whole_table = str(soup.find('div', attrs={"id" : "collapse-1653"}))
-    return whole_table
-  
-  def get_phd_names_from_uni(soup):
-    whole_table = str(soup.find('div', attrs={"id" : "collapse-1656"}))
-    return whole_table
-  
-  def firstname(table):
-    pattern_firstname = '"MX_FIRSTNAME":"(.*?)",'
-    firstname = re.findall(pattern_firstname,table)
-    return firstname
-  
-  def lastname(table):
-    pattern_lastname = '"MX_LASTNAME":"(.*?)",'
-    lastname = re.findall(pattern_lastname,table)
-    return lastname
-  
-  def phd_firstname(table):
-    pattern_firstname = 'MX_FIRSTNAME&quot;:&quot;(.*?)&quot'
-    firstname = re.findall(pattern_firstname, table)
-    return firstname
-  
-  def phd_lastname(table):
-    pattern_lastname = 'MX_LASTNAME&quot;:&quot;(.*?)&quot' 
-    lastname = re.findall(pattern_lastname, table)
-    return lastname
-  
-  
+class ResearcherScraper:
+    BASE_URL = 'https://example.com'
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582'}
+    PROXIES = {'http': os.getenv('HTTP_PROXY')}
+    
+    def __init__(self, uni_id):
+        self.uni_id = uni_id
+    
+    def _get_soup(self, url):
+        html = requests.get(url, headers=self.HEADERS, proxies=self.PROXIES).text
+        return BeautifulSoup(html, 'html.parser')
+    
+    def _get_table(self, collapse_id):
+        url = f'{self.BASE_URL}/tables?id={self.uni_id}&collapse={collapse_id}'
+        soup = self._get_soup(url)
+        table = str(soup.find('div', attrs={'id': f'collapse-{collapse_id}'}))
+        return table
+    
+    def get_professor_names(self):
+        table = self._get_table(1652)
+        pattern_firstname = '"MX_FIRSTNAME":"(.*?)",'
+        pattern_lastname = '"MX_LASTNAME":"(.*?)",'
+        firstnames = re.findall(pattern_firstname, table)
+        lastnames = re.findall(pattern_lastname, table)
+        return list(zip(firstnames, lastnames))
+    
+    def get_postdoc_names(self):
+        table = self._get_table(1653)
+        pattern_firstname = '"MX_FIRSTNAME":"(.*?)",'
+        pattern_lastname = '"MX_LASTNAME":"(.*?)",'
+        firstnames = re.findall(pattern_firstname, table)
+        lastnames = re.findall(pattern_lastname, table)
+        return list(zip(firstnames, lastnames))
+    
+    def get_phd_names(self):
+        table = self._get_table(1656)
+        pattern_firstname = 'MX_FIRSTNAME&quot;:&quot;(.*?)&quot'
+        pattern_lastname = 'MX_LASTNAME&quot;:&quot;(.*?)&quot'
+        firstnames = re.findall(pattern_firstname, table)
+        lastnames = re.findall(pattern_lastname, table)
+        return list(zip(firstnames, lastnames))
+
