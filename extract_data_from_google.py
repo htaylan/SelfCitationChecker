87001b5d4b46e20ca5a75bba59820892a5c0449f
htaylan <htaylanturan@gmail.com>
2023-03-15 12:49:00 +0100

    Update extract_data_from_google.py

--- extract_data_from_google.py
+++ extract_data_from_google.py
@@ -59,3 +59,33 @@ class GoogleSearch:
     def search(self):
         response = self.get_results()
         return self.parse_results(response)
+    
+    def get_researcher_stats(id):
+    
+        html = requests.get(f'https://scholar.google.com/citations?hl=en&user={id}', headers=headers, proxies=proxies)
+        soup = BeautifulSoup(html.text, 'lxml')
+
+        interests = soup.select_one('#gsc_prf_int').text
+
+        for info in soup.select('.gsc_rsb'):
+
+            cite_total = info.select_one('tr:nth-child(1) .gsc_rsb_sc1+ .gsc_rsb_std').text
+            cite_last5 = info.select_one('tr:nth-child(1) .gsc_rsb_std+ .gsc_rsb_std').text
+            h_total = info.select_one('tr:nth-child(2) .gsc_rsb_sc1+ .gsc_rsb_std').text
+            h_last5 = info.select_one('tr:nth-child(2) .gsc_rsb_std+ .gsc_rsb_std').text
+            i_total = info.select_one('tr~ tr+ tr .gsc_rsb_sc1+ .gsc_rsb_std').text
+            i_last5 = info.select_one('tr~ tr+ tr .gsc_rsb_std+ .gsc_rsb_std').text
+            #articles_num = info.select_one('.gsc_rsb_m_a:nth-child(1) span').text.split(' ')[0]
+
+            years = [graph_year.text for graph_year in soup.select('.gsc_g_t')]
+            citations = [graph_citation.text for graph_citation in soup.select('.gsc_g_a')]
+
+            citation_data = []
+
+            for year, citation in zip(years,citations):
+                citation_data.append({
+                  'year': year,
+                  'citation': citation,
+                  })   
+
+        return cite_total, cite_last5, h_total, h_last5, i_total, i_last5
